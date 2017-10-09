230c86500783090a2c754da92bf34a91b7af677d
Mukul <mukul1904@gmail.com>
2017-10-10 00:24:07 +0530

    first working version committed

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict';
 
+/**
+ * @desc This is fun slexa skill for Game Of thrones trivia, where you ask Alexa to give you a fact about
+ * a house of your choice
+ */
+
+const trivia = [
+    {
+        house: 'starks',
+        trivia: 'They live at winterfell'
+    },
+    {
+        house: 'starks',
+        trivia: 'Most of them dies during war of kings'
+    },
+    {
+        house: 'starks',
+        trivia: 'There sigil is wold'
+    },
+    {
+        house: 'starks',
+        trivia: 'They build at wall'
+    }
+];
+
 // --------------- Helpers that build all of the responses -----------------------
 
 function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
@@ -34,6 +58,88 @@ function buildResponse(sessionAttributes, speechletResponse) {
 
 // --------------- Functions that control the skill's behavior -----------------------
 
+function getWelcomeResponse(callback) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Valer Morghulis! Welcome to the Game of Thrones Alexa Skill. ' +
+        'Ask any question related to game of thrones, and you will get your answer.';
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
+    const repromptText = 'Winter is coming! Please ask your question.';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for trying the Game of Thrones Alexa Skill. Valer Dohaeris!';
+    // Setting this to true ends the session and exits the skill.
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function getTriviaByHouse(house) {
+    const list = trivia.filter(item => item.house === house);
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+/**
+ * Sets the color in the session and prepares the speech to reply to the user.
+ */
+function getGoTHouseTrivia(intent, session, callback) {
+    console.log("intent", intent);
+    const cardTitle = intent.name;
+    const houseSlot = intent.slots.House;
+    let repromptText = '';
+    let sessionAttributes = {};
+    const shouldEndSession = false;
+    let speechOutput = '';
+
+    if (houseSlot) {
+        const house = houseSlot.value;
+        const resp = getTriviaByHouse(house);
+        console.log("intent recieved", resp);
+        speechOutput = `Here's a face about house ${house}. ${resp.trivia}`
+        repromptText = "Tired already! I can do this whole day.";
+    } else {
+        speechOutput = "Night is full of terror and darkness! We didn't get what you're asking for";
+        repromptText = "Try something like, Tell me something about Starks";
+    }
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function getColorFromSession(intent, session, callback) {
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+    // Setting repromptText to null signifies that we do not want to reprompt the user.
+    // If the user does not respond or says something that is not understood, the session
+    // will end.
+    callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
 
 // --------------- Events -----------------------
 
@@ -42,6 +148,37 @@ function buildResponse(sessionAttributes, speechletResponse) {
  */
 function onSessionStarted(sessionStartedRequest, session) {
     console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+/**
+ * Called when the user launches the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+    // Dispatch to your skill's launch.
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'GoTTriviaIntent') {
+        getGoTHouseTrivia(intent, session, callback);
+    } else if (intentName === 'AMAZON.HelpIntent') {
+        getWelcomeResponse(callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
 }
 
 /**
@@ -60,20 +197,37 @@ function onSessionEnded(sessionEndedRequest, session) {
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = (event, context, callback) => {
     try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+             callback('Invalid Application ID');
+        }
+        */
 
         if (event.session.new) {
             onSessionStarted({ requestId: event.request.requestId }, event.session);
         }
-        if (!event.request && !event.request.type) {
-            callback('Invalid request');
-        }
-        switch (event.request.type) {
-            case value:
-                
-                break;
-        
-            default:
-                break;
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
         }
     } catch (err) {
         callback(err);
